82abbcc5645479c8513444d7f3b728736a3f97c5
vagrant <vagrant@homestead>
2020-04-07 05:34:19 +0000

    异常处理

---
+++ app/Exceptions/InternalException.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Throwable;
+use Illuminate\Http\Request;
+
+class InternalException extends Exception
+{
+    protected $msgForUser;
+
+    public function __construct($message = "", $msgForUser = "系统内部错误", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->msgForUser = $msgForUser;
+    }
+
+    public function render(Request $request)
+    {
+        if ($request->expectsJson()) {
+            return response()->json(['msg' => $this->msgForUser], $this->code);
+        }
+
+        return view('pages.error', ['msg' => $this->msgForUser]);
+    }
+
+}
