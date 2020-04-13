bddb0a78760cd81950ae33dd6d5f0c8e273bf5c0
vagrant <vagrant@homestead>
2020-04-13 10:00:59 +0000

    购物车页面

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,9 @@ Route::group(['middleware' => ['auth', 'verified']], function(){
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
 
+    Route::get('cart', 'CartController@index')->name('cart.index');
     Route::post('cart', 'CartController@add')->name('cart.add');
+    Route::delete('cart/{productSku}', 'CartController@remove')->name('cart.remove');
 });
 
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
