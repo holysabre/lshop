821d636ef7f1d7b1a411439b70a3dc5050e00aab
vagrant <vagrant@homestead>
2020-04-16 07:09:10 +0000

    用户界面 - 订单列表页

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ Route::group(['middleware' => ['auth', 'verified']], function(){
     Route::post('cart', 'CartController@add')->name('cart.add');
     Route::delete('cart/{productSku}', 'CartController@remove')->name('cart.remove');
 
+    Route::get('orders', 'OrdersController@index')->name('orders.index');
     Route::post('orders', 'OrdersController@store')->name('orders.store');
 
 });
